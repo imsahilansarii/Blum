a42b72734d933b78e16d8a920140a2e919d649a1
Sahil Ansari <58244066+imsahilansarii@users.noreply.github.com>
2024-08-11 17:22:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 // @icon         https://cdn.prod.website-files.com/65b6a1a4a0e2af577bccce96/65ba99c1616e21b24009b86c_blum-256.png
 // @downloadURL  https://github.com/mudachyo/Blum/raw/main/blum-autoclicker.user.js
 // @updateURL    https://github.com/mudachyo/Blum/raw/main/blum-autoclicker.user.js
-// @homepage     https://github.com/mudachyo/Blum
+// @homepage     https://github.com/imsahilansarii/Blum
 // ==/UserScript==
 
 let GAME_SETTINGS = {
